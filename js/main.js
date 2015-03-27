8ba772d09d1e9ece5990cdc0f622b536995a680e
GiarcEnoredlac <calderonecraig@gmail.com>
2015-03-26 23:00:14 -0400

    updated layout

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	console.log('yay this script loaded!!');
+    	event.preventDefault();
+  	    $('.title').effect( "bounce", "slow" );
+  	    $('.nav').effect( "bounce", "slow" );
+});
